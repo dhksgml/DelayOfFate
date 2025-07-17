3817a55b33ca4f809b6e7c4e2b5ce4bf1f0a4e8c
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-07-18 08:41:40 +0900

    0718 - ItemData 수정
    + ItemData 생성할 때, ItemDataTemplate인 Scriptable의 데이터를 복사해서 ItemData 생성

---
+++ Assets/Script/Item/ItemData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Item/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public int id; // 고유 ID
+
+    [Header("이름, 인게임, 아이콘")]
+    public string itemName;
+    public Sprite InGameSprite;
+    public Sprite icon;
+
+    public int Coin;  // 코인
+    public int Weight;
+    [Header("점수")]
+    public int ValPoint;
+
+    [Header("중복형 아이템인가")]
+    public bool Count_Check;
+    public int Count = 1;
+
+    [Header("판매 가능 / 즉시 판매 가능")]
+    public bool Sell_whether;
+    public bool Sell_immediately;
+
+    [Header("사용 가능한 아이템인가")]
+    public bool isUsable;
+    public float Usage_cool_down;
+    public bool Charging;
+
+    [Header("랜덤 값 설정")]
+    public int CoinDeviation; // 오차 값 (±)
+    public int WeightDeviation; // 오차 값 (±)
+    public int CountDeviation; // 오차 값 (±)
+
+    [HideInInspector]
+    public bool Drop_item; // 떨어트린 적이 있는 아이템인가
+}
