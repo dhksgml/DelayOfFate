bbd66832050ba56937ad496c84f59bd78fb76867
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-07-17 15:17:25 +0900

    0717 영혼강화 추가 및 PassiveManager 위치 변경
    + 영혼강화 제련, 다다익선, 등용문, 정정당당(악귀 약점 공격 불가) 제외하고 모두 추가됨.

---
+++ Assets/Script/ItemPassives/NearestItemFinder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NearestItemFinder : MonoBehaviour
+{
+    public LayerMask itemLayer;          // 인스펙터에서 "Item" 레이어 선택
+    public float searchRadius = 20f;     // 탐색 반경
+    public Transform nearestItem;
+
+    public void FindNearestItem()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, searchRadius, itemLayer);
+
+        float closestDistance = Mathf.Infinity;
+        Transform closest = null;
+
+        foreach (Collider2D hit in hits)
+        {
+            float distance = Vector3.Distance(transform.position, hit.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closest = hit.transform;
+            }
+        }
+
+        nearestItem = closest;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+    }
+}
