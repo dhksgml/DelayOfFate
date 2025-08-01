65545652c1bad84d6295102216ed8d4a7cf557ee
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-08-01 11:11:32 +0900

    Stage_Scene 및 TitleScene 변경
    + 일시정지 버튼이 클릭되지 않는 문제 해결
    + 튜토리얼 타이틀 씬으로 분리

--- Assets/Script/TitleUI/MainMenuUIController.cs
+++ Assets/Script/TitleUI/MainMenuUIController.cs
@@ -6,11 +6,15 @@ public class MainMenuUIController : MonoBehaviour
 {
     public void OnClickStartButton(string loadSceneName)
     {
-        if(GameManager.Instance.isTutorial)
-            GameManager.Instance.LoadScene("Tutorial_Scenes");
-        else
-            GameManager.Instance.LoadScene(loadSceneName);
+        GameManager.Instance.LoadScene(loadSceneName);
     }
+
+    public void OnClickTutorialButton()
+    {
+        GameManager.Instance.isTutorial = true;
+        GameManager.Instance.LoadScene("Tutorial_Scenes");
+    }
+
     public void OnClickExitButton()
     {
         GameManager.Instance.QuitGame();
