e3da11543433dc5c577c11f07911a87a98c8bfca
KMJ770 <rlaalswp8524@gmail.com>
2025-08-04 02:34:33 +0900

    탈혼귀 완성
    * 탈혼귀 완성
    * 탈혼귀를 위해 플레이어 내에 빈 오브젝트 하나를 생성하여 플레이어가 탈혼귀 주변에 E키를 누르면 발견되게 해주었음
    * 다만 현재는 바닥에 있는 아이템처럼 정보와 가격이 나오지 않음

---
+++ Assets/Enemy/Script/Tal_hon_gwi/Player_Tal_hon_gwi_Find.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Tal_hon_gwi_Find : MonoBehaviour
+{
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if(collision.CompareTag("Enemy"))
+            {
+                Tal_hon_gwi talhongwi = collision.gameObject.GetComponent<Tal_hon_gwi>();
+
+                if (talhongwi != null)
+                {
+                    if (Input.GetKeyUp(KeyCode.E))
+                    {
+                        talhongwi.isSeek = true;
+                    }
+                }
+            }
+        }
+    }
+}
