e41c2b4925a77eaff856ec69c1918d9ba320c854
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-08-08 19:05:07 +0900

    마우스 휠 방향 반대로 변경

--- Assets/Script/Player/Player_Item_Use.cs
+++ Assets/Script/Player/Player_Item_Use.cs
@@ -130,8 +130,8 @@ public class Player_Item_Use : MonoBehaviour
 
         // 마우스 휠로 슬롯 변경
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll > 0f) selectedSlotIndex = (selectedSlotIndex + 1) % 4;  // 0~3 범위로 돌아가도록
-        if (scroll < 0f) selectedSlotIndex = (selectedSlotIndex + 3) % 4;  // 역방향 스크롤 (0~3 범위로 돌아가도록)
+        if (scroll > 0f) selectedSlotIndex = (selectedSlotIndex + 3) % 4;  // 역방향 스크롤 (0~3 범위로 돌아가도록)
+        if (scroll < 0f) selectedSlotIndex = (selectedSlotIndex + 1) % 4;  // 0~3 범위로 돌아가도록
 
         // selectedSlotIndex가 0~3 범위 내로 유지되도록 보장
         selectedSlotIndex = Mathf.Clamp(selectedSlotIndex, 0, 3);
