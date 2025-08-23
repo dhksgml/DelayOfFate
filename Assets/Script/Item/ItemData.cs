f3e70b9299417f5fb68debcbc89e8aa0023e485b
znlfxm <63768288+znlfxm@users.noreply.github.com>
2025-08-23 19:48:58 +0900

    아이템 정보 변경
    무게와 개수 삭제

--- Assets/Script/Item/ItemData.cs
+++ Assets/Script/Item/ItemData.cs
@@ -9,33 +9,13 @@ public class ItemData : ScriptableObject
     public string itemName;
     public Sprite InGameSprite;
     public Sprite icon;
-
-    [Header("아이템 사용 SP")]
-    public float spendSPAmount;
-
-    [Space(10)]
     public int Coin;  // 코인
-    public int Weight;
-    [Header("점수")]
-    public int ValPoint;
 
-    [Header("중복형 아이템인가")]
-    public bool Count_Check;
-    public int Count = 1;
-
-    [Header("판매 가능 / 즉시 판매 가능")]
+    [Header("판매 가능")]
     public bool Sell_whether;
-    public bool Sell_immediately;
-
-    [Header("사용 가능한 아이템인가")]
-    public bool isUsable;
-    public float Usage_cool_down;
-    public bool Charging;
 
     [Header("랜덤 값 설정")]
     public int CoinDeviation; // 오차 값 (±)
-    public int WeightDeviation; // 오차 값 (±)
-    public int CountDeviation; // 오차 값 (±)
 
     [HideInInspector]
     public bool Drop_item; // 떨어트린 적이 있는 아이템인가
