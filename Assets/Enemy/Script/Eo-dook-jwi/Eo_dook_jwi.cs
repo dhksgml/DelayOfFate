90d5c6f729c519cee55a611c4ea3dc9a9208d7a8
znlfxm <63768288+znlfxm@users.noreply.github.com>
2025-09-05 15:50:27 +0900

    Mjs 3 (#210)
    * 미니맵 크기 줄이기
    
    600 > 300
    
    * 아이콘 제거
    
    인게임 씬도 건들긴 함
    
    * 2트
    
    * Revert "아이콘 제거"
    
    This reverts commit 4b043da41cc583f4f01706ffe15e21dc9fb06000.
    
    * 3트
    
    * 종합변경
    
    너무 많이 건들였다
    
    * 3트
    
    * 4트
    
    * 상점수정
    
    말풍선, 담배 추가, 패시브 칸 위치 변경
    
    * 상점 2트

--- Assets/Enemy/Script/Eo-dook-jwi/Eo_dook_jwi.cs
+++ Assets/Enemy/Script/Eo-dook-jwi/Eo_dook_jwi.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
-using UnityEditor.ShaderGraph.Internal;
+//using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Eo_dook_jwi : Enemy
