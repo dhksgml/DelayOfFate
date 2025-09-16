70b0826c24775aa9baad816dfcf56531231ecfb2
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-09-16 22:22:03 +0900

    상점에서 무기 강제하기 추가,  무기 가격 오류 수정
    + 한글 들어간 파일이 깨지는 경우, VS studio에서 파일 > 다른이름저장 > 저장 옆에 화살표 > 인코딩하여 저장 > UTF-8로 변경
    + shop에서 speech_bubble_on 수정

--- Assets/Script/UI/Stage_Manager.cs
+++ Assets/Script/UI/Stage_Manager.cs
@@ -11,6 +11,30 @@ public class Stage_Manager : MonoBehaviour
     }
     public void Shop_end() // 상점 전부 고른 후 전투씬으로 넘어가기
     {
+        if(GameManager.Instance != null)
+        {
+            int weaponCount = 0;
+
+            if(GameManager.Instance.WeaponData.Length != 2)
+                return;
+
+            for(int i = 0; i < 2; i++)
+            {
+                if (GameManager.Instance.WeaponData[i] == null)
+                    continue;
+                weaponCount += 1;
+            }
+
+            if(weaponCount == 0)
+            {
+                Debug.Log("넌 못 지나간다");
+                Shop shop = FindObjectOfType<Shop>();
+                if (shop != null)
+                    shop.speech_bubble_on("무기");
+                return;
+            }
+        }
+            
         ShopQuickSlot shopQuickSlot = FindObjectOfType<ShopQuickSlot>();
         GameManager.Instance.SlotsData = shopQuickSlot.SlotsData; // 임시 데이터에 있던걸 게임매니저로 옮기기
         GameManager.Instance.LoadScene("InGame_Scenes");
