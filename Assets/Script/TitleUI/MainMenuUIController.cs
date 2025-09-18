522848d478d8ba13f1baf3937e7bb8a363cd27cd
znlfxm <63768288+znlfxm@users.noreply.github.com>
2025-09-18 09:28:11 +0900

    Revert "Merge branch 'main' into MJS_3"
    This reverts commit 8b4cdb8b67353f8d0f419c4848ad4b2a3a0de54d, reversing
    changes made to b13d7366f0aa91614a8859d05e22eb24ca3f6342.

--- Assets/Script/TitleUI/MainMenuUIController.cs
+++ Assets/Script/TitleUI/MainMenuUIController.cs
@@ -1,68 +1,22 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class MainMenuUIController : MonoBehaviour
 {
-	public List<Button> menuButtons; // 순서대로 Start, Tutorial, Exit 버튼 넣기
-	private int currentIndex = 0;
+    public void OnClickStartButton(string loadSceneName)
+    {
+        GameManager.Instance.LoadScene(loadSceneName);
+    }
 
-	void Start()
-	{
-		// 초기 선택
-		if (menuButtons.Count > 0)
-		{
-			EventSystem.current.SetSelectedGameObject(menuButtons[0].gameObject);
-		}
-	}
+    public void OnClickTutorialButton()
+    {
+        GameManager.Instance.isTutorial = true;
+        GameManager.Instance.LoadScene("Tutorial_ShopScenes");
+    }
 
-	void Update()
-	{
-		HandleNavigation();
-		HandleSubmit();
-	}
-
-	private void HandleNavigation()
-	{
-		// 위쪽 입력
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			currentIndex--;
-			if (currentIndex < 0) currentIndex = menuButtons.Count - 1;
-			EventSystem.current.SetSelectedGameObject(menuButtons[currentIndex].gameObject);
-		}
-		// 아래쪽 입력
-		else if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			currentIndex++;
-			if (currentIndex >= menuButtons.Count) currentIndex = 0;
-			EventSystem.current.SetSelectedGameObject(menuButtons[currentIndex].gameObject);
-		}
-	}
-
-	private void HandleSubmit()
-	{
-		if (Input.GetKeyDown(KeyCode.Z))
-		{
-			menuButtons[currentIndex].onClick.Invoke();
-		}
-	}
-
-	// 기존 버튼 처리 메서드들
-	public void OnClickStartButton(string loadSceneName)
-	{
-		GameManager.Instance.LoadScene(loadSceneName);
-	}
-
-	public void OnClickTutorialButton()
-	{
-		GameManager.Instance.isTutorial = true;
-		GameManager.Instance.LoadScene("Tutorial_ShopScenes");
-	}
-
-	public void OnClickExitButton()
-	{
-		GameManager.Instance.QuitGame();
-	}
+    public void OnClickExitButton()
+    {
+        GameManager.Instance.QuitGame();
+    }
 }
