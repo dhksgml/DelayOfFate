5dc3d1debe8118c75c607da09917eeb66a347788
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-09-30 01:32:36 +0900

    플레이어 사망, 부활 오류 수정.

--- Assets/Sprites/UI/Shader/UIGlowController.cs
+++ Assets/Sprites/UI/Shader/UIGlowController.cs
@@ -6,7 +6,7 @@ public class UIGlowController : MonoBehaviour
     [SerializeField] private Image targetImage;
     private Material mat;
 
-    void Start()
+    private void Awake()
     {
         mat = targetImage.material;
     }
