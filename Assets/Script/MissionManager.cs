19f96b47228caf834cdd6e36bd06290aac241094
znlfxm <63768288+znlfxm@users.noreply.github.com>
2025-11-01 23:33:03 +0900

    Mjs 3 (#300)
    * 아이템 변화중
    
    중간세이브
    
    * 아이템 효과 추가중
    
    우선 판매시 효과들 우선으로 추가중
    
    * 키조작쪽 변경
    
    아이템도 변경
    
    * 줍기 문제, ui 문제
    
    근데 줍고 판매가 안되긴함
    
    * 아이템 패시브 ui 적용
    
    * 메인씬에서테스트
    
    * 상점변경
    
    무기, 영혼, 전투 고르기 가능
    
    * 이것저것
    
    상점 위주로 많이 고침
    
    * 조적 ui 수정
    
    공격과 무기 변경키 변경함
    
    * 중간세이브
    
    * 상점 설명 표기
    
    호롱빛은 어쩌지,
    그리고 1일차 교환에도 커서 가는거 어쩌지
    
    * 상점선택색상변경
    
    * 패시브 변경 예정들
    
    * 마무리 작업
    
    회복 방식 수정
    아이템 주전자 추가
    
    * Update MainScene.unity
    
    * 이것저것
    
    공격 범위 증가
    동전 아이템 제작
    그외 이것저것
    
    * 횃불과 배치 수정
    
    횃불 불 매커니즘을 플레이어의 호롱 빛과 비슷하게 수정함(벽에 막힘)
    
    횃불의 위치를 전반적으로 벽에 가깝게 재배치함
    
    위 과정에서 벽뒤가 쪼금 보이긴 하는데 다시 수정하면 될듯
    
    * 하나 놓침
    
    * 복구+이것저것
    
    여러 아이템 추가
    +플레이어 피격, 회복 텍스트 추가 하긴 해야할듯
    ++패시브랑 코드가 충돌? 나서 이속증가가 제대로 안됨
    
    * 이것저것 추가 조정
    
    바로 개발 못하는 애들은 일단 설명 비활성화
    호롱불 빛 조절 완료
    +분열귀 오류 난거 같은데 체크좀
    
    * 상점수정
    
    인게임 빌드시 문제될만한 부분들 못가게 변경
    
    * 무기, 상점 수정
    
    부적, 호리병 변경 수정
    상점에서 아이템 판매 불가능으로 변경
    설명수정
    무기의 수량 시스템 제거
    
    * 호리병 쿨타임 추가
    
    근데 빗나가면 쿨타임 안돌게끔 해야 할꺼 같은데 아직 못함
    
    * 상점수정
    
    * 메인메뉴 ui 편의성 추가
    
    메인메뉴, 결과보고 창 버튼으로 넘기기 가능 (ASDZXC)
    
    * 아이템 수정
    
    아이템 판매류 효과들 발동 안되던 현상 수정, 비녀 판매시 계속 혼으로 들어오던거 수정
    
    * 수정버전
    
    적이 더 많이 나오고 튜토리얼 선택 불가
    
    * 임무 시스템 중간 세이브
    
    여러가지 건듬
    
    * 미션 중간 세이브 2
    
    이제 선택하고
    인게임에 뭐 골랐는지 뜸
    다만 성공여부 체크, 보상 지급은 아직 미개발
    
    * 임무 시스템 중간 세이브 3
    
    비녀 판매시 다른 아이템들도 혼으로 팔리던 증상 해결
    일부 미션 수행 가능함
    
    * 임무 4
    
    혼령강화 보상이 뜨긴하는데
    아직 오류 조금있음

--- Assets/Script/MissionManager.cs
+++ Assets/Script/MissionManager.cs
@@ -22,6 +22,7 @@ public class MissionManager : MonoBehaviour
 
     // 현재 진행중인 미션
     private Mission_System.MissionData activeMission;
+    private string selectedPassiveId = ""; // 미션에서 선택한 혼령강화 ID
     private bool isMissionActive = false; // 미션 진행 중 (인게임에서)
     private bool isMissionSelected = false; // 미션 선택됨 (상점에서)
     private bool isMissionCompleted = false;
@@ -53,8 +54,6 @@ public class MissionManager : MonoBehaviour
 
     void Start()
     {
-        //inGameMissionText = GameObject.Find("Mission_Text").GetComponent<TMP_Text>();
-        //missionUIPanel = GameObject.Find("Mission_Image_bk");
         // 미션 슬롯 초기화
         if (missionSlot1 != null) missionSlot1.GenerateRandomMission();
         if (missionSlot2 != null) missionSlot2.GenerateRandomMission();
@@ -69,16 +68,14 @@ public class MissionManager : MonoBehaviour
         if (IsInShopScene())
         {
             HandleMissionSelection();
-
-            //HideMissionUI(); // 상점에서는 미션 UI 숨김
+            HideMissionUI(); // 상점에서는 미션 UI 숨김
         }
         // 인게임 씬에서는 미션 UI 표시
         else if (IsInGameScene())
         {
-            inGameMissionText = GameObject.Find("Mission_Text").GetComponent<TMP_Text>();
-            missionUIPanel = GameObject.Find("Mission_Image_bk");
+            UpdateInGameMissionUI();
         }
-        UpdateInGameMissionUI();
+
         // 미션 진행 중 시간 체크
         if (isMissionActive && activeMission != null)
         {
@@ -117,7 +114,7 @@ public class MissionManager : MonoBehaviour
             UpdateSelectionUI();
         }
 
-        // Z, X, C,a,s,d 키로 미션
+        // Z, X, C 키로 각 미션 직접 선택
         if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.C)) && !isMissionActive)
         {
             SelectMission(currentSelectedIndex);
@@ -134,7 +131,6 @@ public class MissionManager : MonoBehaviour
         }
     }
 
-    // 미션 선택
     public void SelectMission(int slotIndex)
     {
         Mission_System selectedSlot = null;
@@ -150,20 +146,23 @@ public class MissionManager : MonoBehaviour
 
         // 미션 활성화
         activeMission = selectedSlot.GetCurrentMission();
-        isMissionActive = true;
+        isMissionSelected = true;
+        isMissionActive = false;
         isMissionCompleted = false;
 
-        // 진행도 초기화
-        ResetMissionProgress();
-
-        // 시간 제한 미션이면 시작 시간 기록
-        if (activeMission.type == Mission_System.MissionType.TimeLimit)
+        // 혼령강화 보상이면 예약
+        if (activeMission.rewardType == Mission_System.RewardType.PassiveLow ||
+            activeMission.rewardType == Mission_System.RewardType.PassiveMid ||
+            activeMission.rewardType == Mission_System.RewardType.PassiveHigh ||
+            activeMission.rewardType == Mission_System.RewardType.PassiveMax)
         {
-            missionStartTime = Time.time;
+            selectedPassiveId = activeMission.passiveRewardId; // Mission_System에서 설정한 ID
         }
+
         FindObjectOfType<Stage_Manager>().Quest_ok();
         Debug.Log($"미션 선택됨: {activeMission.type}, 목표: {activeMission.targetCount}");
     }
+
 
     // 미션 진행도 초기화
     void ResetMissionProgress()
@@ -332,9 +331,16 @@ public class MissionManager : MonoBehaviour
         }
     }
 
-    // 미션 실패
+    // 미션 실패 시
     void FailMission()
     {
+        // 혼령강화 예약 취소
+        if (!string.IsNullOrEmpty(selectedPassiveId))
+        {
+            PassiveItemManager.Instance?.CancelPassiveReservation(selectedPassiveId);
+            selectedPassiveId = "";
+        }
+
         isMissionActive = false;
         isMissionSelected = false;
         isMissionCompleted = false;
@@ -403,8 +409,13 @@ public class MissionManager : MonoBehaviour
             case Mission_System.RewardType.PassiveMid:
             case Mission_System.RewardType.PassiveHigh:
             case Mission_System.RewardType.PassiveMax:
-                // TODO: PassiveItemManager를 통해 랜덤 혼령강화 지급
-                Debug.Log($"보상 지급: {activeMission.rewardType} 혼령강화");
+                // 혼령강화 구매 확정
+                if (!string.IsNullOrEmpty(selectedPassiveId))
+                {
+                    PassiveItemManager.Instance?.ConfirmPassivePurchase(selectedPassiveId);
+                    Debug.Log($"보상 지급: 혼령강화 {selectedPassiveId}");
+                    selectedPassiveId = "";
+                }
                 break;
         }
 
@@ -414,6 +425,7 @@ public class MissionManager : MonoBehaviour
         isMissionCompleted = false;
         activeMission = null;
     }
+
 
     // === 외부 접근용 메서드 ===
 
@@ -496,7 +508,7 @@ public class MissionManager : MonoBehaviour
                 break;
 
             case Mission_System.MissionType.LightAllAreas:
-                progressText = $"지역 모두 밝히기";
+                progressText = $"지역 ({lightedAreaCount}/{totalAreaCount})곳 밝히기";
                 break;
 
             case Mission_System.MissionType.SellItems:
