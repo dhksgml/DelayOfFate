9eb5ed8119e970dc59f00972b7a4814a42c186bc
dhksgml <69703890+dhksgml@users.noreply.github.com>
2025-11-08 18:03:55 +0900

    빛 시스템 수정
    + 횃불로 빛 충전 제거
    + 불 꺼지면 빛 회복, 이동속도 증가
    + 불 켜졌을 때 배터리 소모량 수정(배터리가 너무 느리게 감소해서 감소량 증가) 0.1 -> 0.3

--- Assets/Sprites/UI/Shader/UIGlowController.cs
+++ Assets/Sprites/UI/Shader/UIGlowController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class UIGlowController : MonoBehaviour
 {
     [SerializeField] private Image targetImage;
+    [SerializeField] private Image targetFlashlightUIImage;
     private Material mat;
 
     private void Awake()
@@ -16,13 +17,20 @@ public class UIGlowController : MonoBehaviour
         mat.SetFloat("_GlowStrength", active ? 1f : 0f);
     }
 
+    public void SetImageColor(bool active)
+    {
+        targetFlashlightUIImage.color = active ? Color.white : new Color(0.5f, 0.5f, 0.5f);
+    }
+
     private void OnEnable()
     {
         GameEvents.OnClickLenton += SetGlow;
+        GameEvents.OnClickLenton += SetImageColor;
     }
 
     private void OnDisable()
     {
         GameEvents.OnClickLenton -= SetGlow;
+        GameEvents.OnClickLenton -= SetImageColor;
     }
 }
